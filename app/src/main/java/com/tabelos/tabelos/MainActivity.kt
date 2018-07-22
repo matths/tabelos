82c356af860ed8bfcca4b3aade41d6810b6994e6
Matthias Dittgen <matthias.dittgen@valiton.com>
2018-07-22 17:11:16 +0200

    Initial commit

---
+++ app/src/main/java/com/tabelos/tabelos/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.tabelos.tabelos
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import org.jetbrains.anko.*
+import android.support.v4.os.HandlerCompat.postDelayed
+import android.support.v4.os.HandlerCompat.postDelayed
+import kotlinx.coroutines.experimental.Runnable
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        MainActivityUi().setContentView(this)
+
+        val handler = Handler()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+
+    private val runnable = object : Runnable {
+        override fun run() {
+            runServer()
+        }
+    }
+
+    private fun runServer() {
+        val server = embeddedServer(Netty, port = 8080) {
+            routing {
+                get("/") {
+                    call.respondText("Hello World!", ContentType.Text.Plain)
+                }
+                get("/demo") {
+                    call.respondText("HELLO WORLD!")
+                }
+            }
+        }
+        server.start(wait = true)
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
+
+class MainActivityUi : AnkoComponent<MainActivity> {
+
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+
+        relativeLayout {
+            webView {
+                loadUrl("file:///android_asset/index.html")
+            }
+        }
+
+    }
+
+}
