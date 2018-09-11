d4044b9161f483f43572a8c443f226146db575e2
Matthias Dittgen <matthias.dittgen@valiton.com>
2018-09-11 18:33:03 +0200

    Switch to RSA key in keystore to work on actual device

--- app/src/main/java/com/tabelos/MainActivity.kt
+++ app/src/main/java/com/tabelos/MainActivity.kt
@@ -25,21 +25,21 @@ class MainActivity : Activity() {
     }
 
     private fun runServer() {
+
         // download Bouncy Castle Provider 1.46 for JDK 1.5 to 1.8 from https://mvnrepository.com/artifact/org.bouncycastle/bcprov-ext-jdk15on/1.46
         // wget http://central.maven.org/maven2/org/bouncycastle/bcprov-ext-jdk15on/1.46/bcprov-ext-jdk15on-1.46.jar
 
         // use keytool from e.g.legacy JDK 1.6 and create keystore with a self signed certificate
-        // /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Commands/keytool -genkey -alias tabelos -tabelos tabelos.jks -storepass password -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath ~/Desktop/bcprov-ext-jdk15on-1.46.jar
+        // /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Commands/keytool -genkey -keyalg rsa -alias tabelos -keystore tabelos.jks -storepass password -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath ~/Desktop/bcprov-ext-jdk15on-1.46.jar
 
         // copy keystore file into resource path
         // cp tabelos.jks /Users/[UserName]/AndroidStudioProjects/Tabelos/app/src/main/res/raw/tabelos.jks
 
         // read keystore resource to see if file is exported into build
-        // val content = javaClass.getResource("/res/raw/tabelos.jks).readText()
+        // val content = javaClass.getResource("/res/raw/tabelos.jks").readText()
         // println(content)
 
         // inspired by https://stackoverflow.com/questions/36553190/check-in-the-onreceivedsslerror-method-of-a-webviewclient-if-a-certificate-is
-
         try {
             val webServer = WebServer(Constants.HOSTNAME, Constants.PORT)
             val sslServerSocketFactory: SSLServerSocketFactory = NanoHTTPD.makeSSLSocketFactory(Constants.KEYSTORE_PATH, Constants.KEYSTORE_PASSWORD.toCharArray())
