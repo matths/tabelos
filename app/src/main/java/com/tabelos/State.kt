a3454654c8fc1f563843fbb857d44dd2686a9455
Matthias Dittgen <matthias.dittgen@valiton.com>
2018-10-05 09:11:49 +0200

    Initialize tablet list in global State

--- app/src/main/java/com/tabelos/State.kt
+++ app/src/main/java/com/tabelos/State.kt
@@ -1,11 +1,12 @@
 package com.tabelos
 
 object State {
-    val tabletList:TabletList = TabletList();
 
     const val HOSTNAME = "127.0.0.1"
     const val PORT = 8443
     const val KEYSTORE_PATH = "/res/raw/tabelos.jks"
     const val KEYSTORE_PASSWORD = "password"
 
+    var ipAddress:Array<Int>? = null
+    val tabletList:TabletList = TabletList();
 }
